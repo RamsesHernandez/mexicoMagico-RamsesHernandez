a73bd4771e0739bbc218cc5250f914823e4db179
Ramses Hernandez <ramsesabra@hotmail.es>
2022-07-26 22:43:47 -0500

    Desafío: Menú e-commerce
    Desafío: Menú e-commerce

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cartWidget from '../images/cartWidget.png'
+
+const CartWidget = () => {
+    return(
+        <div>
+            <img src={cartWidget} alt="carrito"></img>
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+import CartWidget from '../CartWidget/CartWidget'
+
+const NavBar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Aulex</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">Why Us</Nav.Link>
+                        <NavDropdown title="Products" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Food</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Imports</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Contact</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More about us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <CartWidget />
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
